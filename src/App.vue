2a1fe4b97c8417ae5eb7979a2c17485dbcd7b2b8
min6683 <dev.sm6683@gmail.com>
2022-04-08 13:10:49 +0900

    DB 수정

--- src/App.vue
+++ src/App.vue
@@ -1,154 +1,220 @@
 <template>
   <div class="container">
-    <h1>Todo List</h1>
+    <h1>TODO LIST</h1>
+
     <!-- 할일 검색 입력창 -->
-    <input class="form-control mb-2" v-model="SearchText" type="text" placeholder="Search Todo List">
-    <div class="red">{{error}}</div>
-    <hr/>
+    <input class="form-control" v-model="searchText" type="text" placeholder="Search Todo list">
+    <div class="red">{{ error }}</div>
+    <hr />
+
     <!-- 할일 추가 입력창 -->
-    <TodoSimpleForm v-on:add-todo="AddTodo" />
+    <TodoSimpleForm v-on:add-todo="addTodo" />
 
+    <!-- 목록없음 안내창 -->
+    <div v-show="!todos.length" class="red">생성된 Todo 목록이 없습니다.</div>
 
-    <!-- 목록 없음 안내창 -->
-    <div v-if="!todos.length" class="mt-2">
-      생성된 Todo 목록이 없습니다.
-    </div>
+    <!-- todo 목록창 -->
+    <TodoList v-bind:todos="todos" v-on:toggle-todo="toggleTodo" v-on:delete-todo="deleteTodo"/>
 
-    <!-- Todo 목록창  -->
-    <TodoList v-bind:todos="FilteredTodo" v-on:toggle-todo="ToggleTodo" v-on:delete-todo="DeleteTodo"/>
-  </div>  
+    <hr />
+
+    <!-- 페이지네이션 -->
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        
+        <li 
+          class="page-item"
+          v-if="nowPage !== 1"
+        >
+          <a class="page-link" v-on:click="getTodo(nowPage - 1)" style="cursor:pointer">Previous</a>
+        </li>
+        
+        <li 
+          class="page-item"
+          v-for="count in numberOfPages"
+          v-bind:key="count"
+
+          v-bind:class="nowPage === count ? 'active': '' "
+        >
+          <a class="page-link" v-on:click="getTodo(count)" style="cursor:pointer">{{count}}</a>
+        </li>
+        
+        <li 
+          class="page-item"
+          v-if="nowPage !== numberOfPages"
+        >
+          <a class="page-link" v-on:click="getTodo(nowPage + 1)" style="cursor:pointer">Next</a>
+        </li>
+      </ul>
+    </nav>
+
+  </div>
 </template>
 
 <script>
-  import {computed, ref} from 'vue'
+  import { computed, ref, watch} from 'vue';
   import axios from 'axios'
 
   import TodoSimpleForm from './components/TodoSimpleForm.vue'
   import TodoList from './components/TodoList.vue'
 
   export default {
+
     components: {
       TodoSimpleForm,
       TodoList
     },
-    setup() {
-      // 할일 목록(배열)을 저장
-      const todos = ref([{
-        id: 1,
-        subject: '할일',
-        complete: false
-      }]);
+    setup() {  
 
-      // 에러메세지
+      // 할일 목록(배열)을 저장
+      const todos = ref([]);
+
+      // 에러 메시지 
       const error = ref('');
 
-      // DB에서 자료 불러오기
-      const GetTodo = async() => {
+      // 페이지네이션 관련
+      // 전체 todos 개수 필요
+      const totalTodos = ref(0);
+      // 한 화면당 보여줄 todo 
+      const limit = 5;
+      // 현재 보여주는 페이지 번호
+      const nowPage = ref(1);
+
+      // 총페이지 숫자
+      const numberOfPages = computed( () => {
+        // 총 게시물 / 페이지당 출력 수  ====> 올림
+        return Math.ceil(totalTodos.value / limit);
+
+      });
+
+      // 할일 검색 관련 
+      const searchText = ref('');
+      watch(searchText, () => {
+        getTodo(1)
+      });
+
+      
+      // DB 에서 자료 불러오기
+      const getTodo = async (page = nowPage.value) => {
         error.value = '';
-        try{
-          // 서버에서 자료를 요청한 후에 결과를
-          // res에서 받는다. (response)
-          const res = await axios.get('http://localhost:3000/todos');
-          // response가 될 때 res라는 객체에 .data를 화면에 보여줄 목록으로 출력한다.
+        // 전달 된 값을 현재 페이지 번호로 받아들인다.
+        nowPage.value = page;
+        try {
+          // 서버에서 자료를 요청을 진행 후에 결과를 
+          // res 에서 받는다. (response)
+          const res = await axios.get(`http://localhost:3000/todos?subject_like=${searchText.value}&_page=${page}&_limit=${limit}&_sort=id&_order=desc`);
+          // console.log(res.headers)
+          // 총 todos 개수 파악
+          totalTodos.value = res.headers["x-total-count"];
+
+          if(numberOfPages.value < nowPage.value) {
+            getTodo(nowPage.value - 1);
+            return;
+          }
+
+          // response 가 될때 res라는 객체에 .data 를 화면에 보여줄
+          // 목록으로 출력한다.
           todos.value = res.data;
-        }catch(err) {
+
+        }catch (err) {
           console.log(err);
-          error.value = "자료를 불러오는데 실패했습니다."
+          error.value = "자료를 불러오는데 실패했습니다.";
         }
       }
 
-      // 최초의 데이터를 호출한다.
-      GetTodo();
+      // 최초에 데이터를 호출한다.
+      getTodo();
 
-      // TodoSimpleForm에서 
-      // add-todo 이벤트로 전달된 객체를
-      // 처리해 주는 콜백 함수
-      const AddTodo = async (todo) => {
-
+      // TodoSimpleForm 에서  
+      // add-todo 이벤트로 전달된 객체를 
+      // 처리해 주는 콜백함수 
+      const addTodo = async (추가되는할일) => {
         error.value = '';
         try {
-            // DB에 저장이 되어야 하는 데이터
-            const res = await axios.post('http://localhost:3000/todos',{
-            subject: todo.subject,
-            complete: todo.complete
+          // 데이터 베이스에 저장이 되어야 하는 데이터
+          await axios.post('http://localhost:3000/todos', {         
+            subject: 추가되는할일.subject,
+            complete: 추가되는할일.complete 
           });
-          todos.value.push(res.data);
+
+          getTodo(1);
+
         }catch(err) {
           console.log(err);
-          error.value = "서버를 확인해 주세요.";
+          error.value = "서버 확인해 주세요.";
         }
-        
-      }
+
+      };
 
       const toggleTodo = async (index) => {
-        // complete 를 업데이트 하겠다
-        // id를 통해서 내용을 업데이트 하겠다.
-        error.value ='';
+
+        // complete 를 업데이트 하겠다.
+        // id 를 통해서 내용을 업데이트 하겠다.
+        error.value = '';
         const id = todos.value[index].id;
         try {
-          // 서버의 DB를 업데이트 한다.
+          
+          // 서버의 DB 를 업데이트 한다.
           await axios.patch('http://localhost:3000/todos/' + id, {
             complete : !todos.value[index].complete
           });
-          // 웹브라우저의 todo의 화면을 표현한다.
+          // 웹브라우저의 todo 의 화면을 표현한다.
           todos.value[index].complete = !todos.value[index].complete;
+
         }catch(err) {
           console.log(err);
-          error.value = "업데이트에 실패하였습니다."
+          error.value = "업데이트에 실패하였습니다.";
         }
+
+
+
+        // console.log('원본 : ' + todos.value[index].complete);
         // todos.value[index].complete = !todos.value[index].complete;
+        // console.log('변경 : ' + todos.value[index].complete);
       }
 
-      const DeleteTodo = async(index) => {
-        // console.log('지우기' + index);
-        // 배열 내에서 순서 번호로부터 1개 제거
+      const deleteTodo = async (index) => {
+       
         const id = todos.value[index].id;
+        // console.log(id);
         error.value = '';
+        
         try {
-          // 전체 삭제가 아니라 id와 같은 DB를 삭제
-          await axios.delete('http://localhost:3000/todos/' + id);
-          todos.value.splice(index,1);
-        }catch(err){
+          // 전체 삭제가 아니라 id와 같은 DB 를 삭제
+          await axios.delete('http://localhost:3000/todos/' + id);          
+          getTodo(nowPage.value);
+        }catch(err) {
           console.log(err);
           error.value = "삭제에 실패했습니다.";
         }
-      }
 
-      // 할일 검색 관련
-      const SearchText = ref('');
-
-      // 검색에 따른 목록을 갱신해 주는 기능을 생성
-      const FilteredTodo = computed(() => {
-        // 만약에 SearchText 와 동일한 todos 목록에 있는 지를 검사해서
-        // 화면에 출력할 예정임.
-        if(SearchText.value) {
-          return todos.value.filter( (todoitem) => {
-            // aaa 객체의 제목에 포함이 되어 있는가? 검사하기
-            return todoitem.subject.includes(SearchText.value);
-          });
-        }
-        return todos.value;
-      });
-
+      }     
+      
       return {
         todos,
-        AddTodo,
-        DeleteTodo,
+        addTodo,
         toggleTodo,
-        SearchText,
-        FilteredTodo,
-        error
+        deleteTodo,
+        
+        searchText,
+
+        error,
+        nowPage,
+        totalTodos,
+        numberOfPages,
+        getTodo
       }
     }
   }
 </script>
 
 <style>
-.red {
-  color: red;
-}
-.todocss {
-  color: gray;
-  text-decoration: line-through;
-}
+  .red {
+    color: red;
+  }
+
+  .todocss {
+    color: gray;
+    text-decoration: line-through;
+  }
 </style>
